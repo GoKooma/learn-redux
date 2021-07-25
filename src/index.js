77e89fb651d5ddb6469ec2df68f75580bfae6a05
KweonMo Koo <mikekoo2010@gmail.com>
2021-07-24 21:32:52 -0700

    Create practice code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+// Example store
+// let store = createStore(reducer)
+
+// STORE -> Globalized state
+
+
+// ACTION -> INCREMENT
+const increment = () => {
+  return {
+    type: "INCREMENT" // same as name
+  }
+}
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+// REDUCER -> How ACTIONS transform the current state to the next state
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH -> Sends the ACTION to REDUCER
+store.dispatch(increment());
+store.dispatch(decrement())
+store.dispatch(decrement())
 
 ReactDOM.render(
   <React.StrictMode>
